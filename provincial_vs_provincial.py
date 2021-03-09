87ea3ad8e7b937dcdda49662e683f383dd9b6e9f
dannyallover <dannyhalawi15@gmail.com>
2021-03-09 02:35:50 -0800

    provincial can now play provincial

--- provincial_vs_provincial.py
+++ provincial_vs_provincial.py
@@ -11,11 +11,15 @@ def get_card_set(cards):
             dict[key] = val
     return dict
 
-card_set = get_card_set(["Witch", "Council Room", "Militia", "Village", "Gardens", "Chapel", \
-                        "Artisan", "Sentry", "Bureaucrat", "Cellar"])
+card_set = get_card_set(["Library", "Mine", "Village", "Militia", "Council Room", "Witch", "Cellar", \
+                        "Gardens", "Artisan", "Sentry"])
 
-buy_menu= [('Gold', 6, 99), ('Witch', 5, 1), ('Council Room', 5, 5), ('Militia', 4, 1), \
+buy_menu_one = [('Gold', 6, 99), ('Witch', 5, 1), ('Council Room', 5, 5), ('Militia', 4, 1), \
             ('Silver', 3, 1), ('Village', 3, 5), ('Silver', 3, 99)]
+
+buy_menu_two = [('Gold', 6, 99), ('Mine', 5, 1), ('Silver', 3, 2), ('Library', 5, 1), \
+            ('Village', 3, 1), ('Mine', 5, 1), ('Village', 3, 1), ('Library', 5, 1), \
+            ('Village', 3, 2), ('Silver', 3, 99)]
 
 if __name__ == '__main__':
     """
@@ -26,7 +30,7 @@ if __name__ == '__main__':
     big_money_win_count = 0
     for i in range(0, num_games):
         game = Game(
-            agents=[ProvincialAgent(buy_menu = buy_menu), BigMoneyAgent()],
+            agents=[ProvincialAgent(buy_menu = buy_menu_one), ProvincialAgent(buy_menu = buy_menu_two)],
             kingdoms=[card_set],
         )
         _, player = game.run()
@@ -36,7 +40,7 @@ if __name__ == '__main__':
             big_money_win_count += 1
         print("game over2")
 
-    print("Provincial won ", provincial_win_count, " many times.")
-    print("Big Money won ", big_money_win_count, " many times.")
+    print("Provincial Buy Menu One won ", provincial_win_count, " many times.")
+    print("Provincial Buy Menu Two won ", big_money_win_count, " many times.")
 
     # print(res)
